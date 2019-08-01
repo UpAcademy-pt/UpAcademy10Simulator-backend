d9d4aed4f5b13b3c5ac1e3c7c6b14c4e7b736b0f
João404 <joaoltxs@hotmail.com>
2019-08-01 16:18:19 +0100

    CRUD
    CRUD created for project

--- src/main/java/simSalProject/repositories/UserRepository.java
+++ src/main/java/simSalProject/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package simSalProject.repositories;
 
+import java.util.List;
+
 import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
+import javax.persistence.TypedQuery;
 
 import simSalProject.models.User_;
 
@@ -13,6 +16,27 @@ public class UserRepository extends EntityRepository<User_>{
 	
 	public Class<User_> getEntityClass(){
 		return User_.class;
+	}
+
+	@Override
+	protected String getAllIds() {
+		// TODO Auto-generated method stub
+		return User_.ALL_USER_IDS;
+	}
+
+	@Override
+	protected String getAllValues() {
+		// TODO Auto-generated method stub
+		return User_.ALL_USER_VALUES;
+	}
+	
+	public List<User_> getUserById (long id){
+		TypedQuery<User_> query = entityManager.createNamedQuery(User_.GET_USER_BY_ID, User_.class);
+		query.setParameter("id", id);
+		
+		return query.getResultList();
+		
+		
 	}
 
 }
