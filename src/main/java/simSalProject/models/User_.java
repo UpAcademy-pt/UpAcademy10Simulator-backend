d4ec58e9a18d21c2a2a39023adc51bbe79fc829a
Ricardo <ricardomartins216@gmail.com>
2019-08-01 16:26:10 +0100

    Update User_ Password

--- src/main/java/simSalProject/models/User_.java
+++ src/main/java/simSalProject/models/User_.java
@@ -1,8 +1,8 @@
 package simSalProject.models;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 
@@ -17,23 +17,25 @@ public class User_ extends Entity_{
 	}
 	
 	private String email;
-	private String password;
+	private Integer password;
 	private UserRole userRole;
+	private long date;
+	
 	
 	public User_() {}
 
 	public User_(String email, String password, UserRole role ) {
 		this.email = email;
-		this.password = password;
+		this.password = password.hashCode();
 		this.userRole = role;
+		this.date = new Date().getTime();
+		
+
+
 	}
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 
@@ -41,11 +43,19 @@ public class User_ extends Entity_{
 		return email;
 	}
 
-	public String getPassword() {
+	public int getPassword() {
 		return password;
 	}
 	
 	public UserRole getUserRole() {
 		return userRole;
 	}
+	
+
+	public int myHashCode(int password) {
+		System.out.println(this.date);
+		System.out.println("Entrou: "+ Objects.hash(password, this.date));
+		return Objects.hash(password, this.date);
+	}
+	
 }
