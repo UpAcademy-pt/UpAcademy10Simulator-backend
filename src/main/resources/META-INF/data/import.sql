d9683a9f0f18324c052ac564b5d2d372aa79b804
Ricardo <ricardomartins216@gmail.com>
2019-08-19 19:01:23 +0100

    Test charset

--- src/main/resources/META-INF/data/import.sql
+++ src/main/resources/META-INF/data/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Telem�vel",false,false,false,false,true,false);
-INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Ve�culo",false,false,false,true,true,false);
-INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Combust�vel",false,false,false,true,true,false);
-INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Internet M�vel",false,false,false,false,true,false);
+INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Telemóvel",false,false,false,false,true,false);
+INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Veículo",false,false,false,true,true,false);
+INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Combustível",false,false,false,true,true,false);
+INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Internet Móvel",false,false,false,false,true,false);
 INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Via Verde",false,false,false,true,true,false);
-INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Manuten��o de Ve�culo",false,false,false,true,true,false);
+INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Manutenção de Veículo",false,false,false,true,true,false);
 
 INSERT INTO Tax(name, value) VALUES("autonomousTributation", 10);
 INSERT INTO Tax(name, value) VALUES("workerSocialSecurity", 11);
