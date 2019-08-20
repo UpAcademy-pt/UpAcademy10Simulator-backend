126a0b6be5403888fa7dbfb4f143a8a74d82b448
c-simoes <52419438+c-simoes@users.noreply.github.com>
2019-08-20 22:39:22 +0100

    .

--- src/main/resources/META-INF/data/import.sql
+++ src/main/resources/META-INF/data/import.sql
@@ -4,6 +4,8 @@ INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Co
 INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Internet Movel",false,false,false,false,true,false);
 INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Via Verde",false,false,false,true,true,false);
 INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Manutencao de veiculo",false,false,false,true,true,false);
+INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Outros C/ Tributacao Autonoma",false,false,false,true,false,false);
+INSERT INTO SimulationFields(name, SA, IRS, SS, TA, BE, varComponent) VALUES("Outros S/ Tributacao Autonoma",false,false,false,false,false,false);
 
 INSERT INTO Tax(name, value) VALUES("autonomousTributation", 10);
 INSERT INTO Tax(name, value) VALUES("workerSocialSecurity", 11);
