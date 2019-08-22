530b9a9586046cc014939b8b60b5bd1451d1e40c
João404 <joaoltxs@hotmail.com>
2019-08-22 20:19:26 +0100

    Organized imports

--- src/main/java/simSalProject/models/Simulation.java
+++ src/main/java/simSalProject/models/Simulation.java
@@ -2,7 +2,6 @@ package simSalProject.models;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
--- src/main/java/simSalProject/models/SimulationDTO.java
+++ src/main/java/simSalProject/models/SimulationDTO.java
@@ -1,6 +1,5 @@
 package simSalProject.models;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
