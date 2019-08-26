48c5b887618bf576e323d7400ea898859b886ecb
João404 <joaoltxs@hotmail.com>
2019-08-26 18:33:02 +0100

    Update EntityRepository.java

--- src/main/java/simSalProject/repositories/EntityRepository.java
+++ src/main/java/simSalProject/repositories/EntityRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 import simSalProject.models.Entity_;
 
